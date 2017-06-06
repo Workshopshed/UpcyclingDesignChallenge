7cb414beaeaaf169f2206046f3961c8ec4355dd6
Andy <andy@workshopshed.com>
2017-06-06 21:02:30 +0100

    test9
    Swapped to using separate streams

--- Tests/test9.js
+++ Tests/test9.js
@@ -1,7 +1,21 @@
-var stream = require('stream');
-var util = require('util');
-var fileduplex = require('file-duplex');
+var fs = require('fs');
 
-var mcu = new fileduplex('/dev/ttymcu0');
+var mcuR = fs.createReadStream('/dev/ttymcu0');
+var mcuW = fs.createWriteStream('/dev/ttymcu0');
+var debug = fs.createReadStream('/dev/ttymcu1');
+
+debug.on('data', function(data) {
+   console.log("Debug:%s",data.toString());
+    });
+
+mcuR.on('data', function(data) {
+   console.log(data.toString());
+    });
+
+setTimeout(function() {mcuW.write('M\n'); },50);
+setTimeout(function() {mcuW.write('U\n'); },100);
+setTimeout(function() {mcuW.write('D\n'); },200);
+setTimeout(function() {mcuW.write('C\n'); },500);
+
 
 
