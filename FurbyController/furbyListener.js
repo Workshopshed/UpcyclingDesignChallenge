df0ce0608aa3957acfcca6b66c0996648d261253
Andy <andy@workshopshed.com>
2017-06-08 06:35:53 +0100

    furby Controller
    Code for controlling the Furby

---
+++ FurbyController/furbyListener.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+// Listens for messages on a queue and responds
+
+var mqtt = require('mqtt');
+
+var controller = require('./furbyController.js');
+
+console.log('Starting Furby Listener');
+
+console.log('Connecting to Queue');
+
+var options = {
+  port:1884,
+  rejectUnauthorized: false,
+  protocol: 'wss'
+}
+
+var client = mqtt.connect('mqtts://eddie.local', options);
+
+client.on('connect', function () {
+    console.log('Connected');
+    client.subscribe('E14_UCDC/+/Events')
+})
+
+client.on('error', function (errMess) {
+    console.log(errMess);
+})
+
+client.on('close', function () {
+    console.log('Closed');
+})
+
+client.on('message', function (topic, message) {
+    var command = JSON.parse(message.toString());
+    controller.doMovement(command);
+    console.log(message.toString());
+})
+
+process.on('SIGINT', function () {
+    console.log("Shutting down SIGINT (Ctrl-C)");
+    controller.shutdown();
+    client.end()
+    process.exit();
+})
+
+function doStuff() {
+    console.log("Heartbeat...");
+};
+
+function run() {
+    console.log('Running')
+    setInterval(doStuff, 30000);
+};
+
+run();
+
---
+++ FurbyController/motorFurby.js
@@ -0,0 +1,120 @@
+var mraa = require('mraa');
+
+    var Motor = function (D1Pin, D2Pin, SpeedPin, HomePin, SensePin, mcuCommands, mcuMessages) {
+    var self = this;
+
+    self.MotorPin1 = new mraa.Gpio(D1Pin);
+    self.MotorPin1.dir(mraa.DIR_OUT);
+    self.MotorPin2 = new mraa.Gpio(D2Pin);
+    self.MotorPin2.dir(mraa.DIR_OUT);
+    self.MotorPinSpeed = new mraa.Pwm(SpeedPin);  //PWM available on default swizzler positions. (3,5,6,9)
+    self.MotorPinSpeed.period_us(700);
+    self.MotorPinSpeed.enable(true);
+    self.mcuCommands = mcuCommands;
+    self.mcuMessages = mcuMessages;
+    self.positionDelta = 10;  //How many counter positions is close enough?
+    self.counter = 0;
+    self.max = 0;
+    self.target = -1;    //What counter position to goto
+    self.mcuCommands.write("M"); //Ask the MCU to get the maximum value
+
+    self.mcuMessages.on('data', function(data) {
+    switch (data.substring(0,1)) {
+        case "C": {
+            self.counter = data.substring(1);
+            if (self.InPosition()) {
+                console.log("Position found at %d", self.counter);
+                self.Stop();
+                self.target = -1;  
+            }
+            setTimeout(function() {self.mcuCommands.write("C") },10);
+            break;
+        }
+        case "M": {
+            self.max = data.substring(1);
+            break;
+        }
+        default:  
+            console.log("Unexpected data recieved %s",data);
+    }
+    });
+
+    self.mcuMessageson('error', function(error) {  
+        console.log('Furby Motor MCU error occurred %s', error);
+    });
+
+    self.Reset = function() {
+        //Set the motor running for long enough to trigger a reset then stop
+        var timeForOneRev = 1000;
+        self.Forward();
+        setTimeout(function() { self.Stop() },timeForOneRev);
+        setTimeout(function() { self.mcuCommands.write("C") },timeForOneRev);
+    }
+
+    self.Forward = function() {
+        self.mcuCommands.write("U");
+        self.MotorPin1.write(1);
+        self.MotorPin2.write(0);
+    }
+
+    self.Reverse = function() {
+        self.mcuCommands.write("D");
+        self.MotorPin1.write(0);
+        self.MotorPin2.write(1);
+    }
+
+    self.Stop = function() {
+        self.target=-1;
+        self.MotorPin1.write(0);
+        self.MotorPin2.write(0);
+    }
+
+    self.Speed = function(percent) {
+        self.MotorPinSpeed.write(percent);
+    }
+
+    self.Position = function() {
+        return self.counter;
+    }
+
+    self.Max = function() {
+        return self.max;
+    }
+
+    self.Distance = function(newPosition) {
+        //What is the distance from the current position to the new position
+        var p = self.Position();
+        var m = self.Max();
+        var D1 = newPosition - p;
+        var D2 = (m - p) + newPosition;
+        if (Math.abs(D1) < Math.abs(D2))
+            return D1;
+        else
+            return D2;
+    }
+
+      self.Direction = function(newPosition) {
+            return Math.sign(self.Distance(newPosition))
+      }
+
+      self.InPosition = function() {
+          if (self.target == -1) return false; //Not going to a position
+          return ((self.Distance() * self.Direction()) < self.positionDelta);
+      }
+
+      self.Goto = function(newPosition) {
+            self.target = newPosition;
+            if (self.InPosition()) {
+                self.target=-1;
+                return;
+            }
+            if (self.Direction(newPosition) > 0) {
+                self.Forward();
+            }
+            else {
+                self.Reverse();
+            }
+      }
+};
+
+    module.exports = Motor;
