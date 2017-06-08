cb9533781a3833e03ac290df118ee4a93290e01f
Andy <andy@workshopshed.com>
2017-06-08 22:52:35 +0100

    carController
    Added in event emitter so we can send messages back to the listener
    Added in bump code from the test (probably should move that to a new
    class.

--- CarController/carController.js
+++ CarController/carController.js
@@ -1,4 +1,7 @@
-var queue = require('queue');
+var queue        = require('queue');
+var util         = require("util");
+var mraa = require('mraa');
+var EventEmitter = require("events").EventEmitter;
 
 var controller = function() {
 
@@ -48,11 +51,47 @@ var controller = function() {
         //Todo: this is where we'd talk to the hardware
         console.log(action);
         console.log(timeout);
+
+	//Tell the listener the action has happened
+        this.emit('action',action);
    }
+
+   var bumpPrevious = -1;
+   var analogPin0 = new mraa.Aio(0); //setup access analog input pin 0
+
+   //Monitor the bumper analogue pin and raise events if it changes
+   setInterval(function () {
+      var analogValueFloat = analogPin0.readFloat();
+      var newval = floatToSwitch(analogValueFloat)
+      if (newval != bumpPrevious) {
+          this.emit('action','bumper' + newval.toString());
+          bumpPrevious  = newval;
+      }
+   } , 100);
+
+  var BumpEnum = {
+    None: 0,
+    Left: 1,
+    Right: 2,
+    Both: 3
+  };
+
+  //Scale the input to one of our 4 values depending on
+  //which of the two switches are pressed
+  function floatToSwitch(value) {
+      if (value < 0.45) return BumpEnum.Both;
+      if (value < 0.58) return BumpEnum.Left;
+      if (value < 0.83) return BumpEnum.Right;
+      if (value >= 0.83) return BumpEnum.None;
+      return 4;
+  }
 
     this.shutdown = function() {
         //Todo: If we need to shutdown any hardware handles etc.
     }
 }
+
+1
+util.inherits(controller, EventEmitter);
 
 module.exports = new controller();
