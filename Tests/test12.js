e6b33b74c0f427a0795a5a976088ae7a4ab53cf9
Andy <andy@workshopshed.com>
2017-06-12 22:19:50 +0100

    test12 - Added motorFurby

--- Tests/test12.js
+++ Tests/test12.js
@@ -1,35 +1,23 @@
-//Test with the MiniBreakout / Outputs
-//Todo: Modify this to use the motorFurby class
+//Test Furby Motor with the MiniBreakout / Outputs
+var motor = require('./motorFurby.js');
+var fs = require('fs');
 
-var Furby = require('./motorFurby.js');
+var mcuR = fs.createReadStream('/dev/ttymcu0');
+var mcuW = fs.createWriteStream('/dev/ttymcu0');
 
-var M1 =    new mraa.Gpio(19);
-var M2 =    new mraa.Gpio(20);
-var speed = new mraa.Pwm(21);  
+var debug = fs.createReadStream('/dev/ttymcu1');
 
-setInterval(function () {
-        value = value + step;
-        if (value <= 0 || value >= 1) {
-            step = -step;
-        }
-        speed.write(value);
-    } , 20);
-
-var ledState = true;
-
-setInterval(function () { 
-	M1.write(ledState?1:0);
-        M2.write(ledState?1:0);
-	ledState = !ledState;
-}, 1000);
-
-setInterval(function () {} , 1000); //Do nothing, stops the apps exiting
-
-process.on('SIGINT', function () {
-    console.log("Shutting down SIGINT (Ctrl-C)");
-    speed.write(0);
-    M1.write(0);
-    M2.write(0);
-    process.exit();
+debug.on('data', function(data) {
+   console.log("Debug:%s",data.toString());
 });
 
+var m = new motor (19, 20, 21, 9, 8, mcuW, mcuR);
+
+m.Speed(0.8);
+m.Reset();
+
+//Todo: Ensure that the one revolution timeout is configured correctly
+setTimeout(function() {m.Goto(100); },5000);
+setTimeout(function() {m.Goto(150); },10000);
+setTimeout(function() {m.Goto(200); },15000);
+setTimeout(function() {m.Goto(50);  },20000);
