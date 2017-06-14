b7ae97f5621ca92e53b918c80919b047252fe35c
Andy <andy@workshopshed.com>
2017-06-14 15:37:35 +0100

    bumper

---
+++ CarController/bumper.js
@@ -0,0 +1,56 @@
+var util         = require("util");
+var mraa = require('mraa');
+var EventEmitter = require("events").EventEmitter;
+
+var bumper = function() {
+
+   var bumpfPrevious = -1;
+   var bumpbPrevious = -1;
+   var bumpfPin = new mraa.Aio(0); //setup access analog input pin 0 (Front bumper)
+   var bumpbPin = new mraa.Aio(1); //setup access analog input pin 1 (Back bumper)
+
+   //Monitor the bumper analogue pin and raise events if it changes
+   setInterval(function () {
+      var analogValueFloat = bumpfPin.readFloat();
+      var newval = floatToSwitch(analogValueFloat)
+      if (newval != bumpfPrevious) {
+          this.emit('bumper','bumperf' + newval.toString());
+          bumpfPrevious  = newval;
+      }
+   } , 100);
+
+   setInterval(function () {
+      var analogValueFloat = bumpbPin.readFloat();
+      var newval = floatToSwitch(analogValueFloat)
+      if (newval != bumpbPrevious) {
+          this.emit('bumper','bumperb' + newval.toString());
+          bumpbPrevious  = newval;
+      }
+   } , 100);
+
+  var BumpEnum = {
+    None: 0,
+    Left: 1,
+    Right: 2,
+    Both: 3
+  };
+
+  //Scale the input to one of our 4 values depending on
+  //which of the two switches are pressed
+  function floatToSwitch(value) {
+      if (value < 0.45) return BumpEnum.Both;
+      if (value < 0.58) return BumpEnum.Left;
+      if (value < 0.83) return BumpEnum.Right;
+      if (value >= 0.83) return BumpEnum.None;
+      return 4;
+  }
+
+    this.shutdown = function() {
+        //Todo: If we need to shutdown any hardware handles etc.
+    }
+}
+
+1
+util.inherits(bumper, EventEmitter);
+
+module.exports = new bumper();
