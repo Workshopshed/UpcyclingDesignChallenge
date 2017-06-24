793bbeb2383f8011b625a8a9691bd59b7c8b743a
Andy <andy@workshopshed.com>
2017-06-24 21:35:00 +0100

    bumper
    Reversed supply voltage

--- CarController/bumper.js
+++ CarController/bumper.js
@@ -13,8 +13,9 @@ var bumper = function() {
    setInterval(function () {
       var analogValueFloatF = bumpfPin.readFloat();
       var newvalF = floatToSwitch(analogValueFloatF);
+      console.log(newvalF);
       if (bumpfPrevious == -1) {
-          bumpfPrevious  = newvalF;
+          bumpfPrevious = newvalF;
       }      
       if (newvalF != bumpfPrevious) {
           self.emit('bumper','bumperf' + newvalF.toString());
@@ -41,15 +42,14 @@ var bumper = function() {
     Both: 3
   };
 
-  //Scale the input to one of our 4 values depending on
-  //which of the two switches are pressed
-  function floatToSwitch(value) {
-      if (value < 0.45) return BumpEnum.Both;
-      if (value < 0.58) return BumpEnum.Left;
-      if (value < 0.83) return BumpEnum.Right;
-      if (value >= 0.83) return BumpEnum.None;
-      return 4;
-  }
+//Scale the input to one of our 4 values depending on
+//which of the two switches are pressed
+function floatToSwitch(value) {
+    if (value < 0.1) return BumpEnum.None;
+    if (value < 0.25) return BumpEnum.Left;
+    if (value < 0.38) return BumpEnum.Right;
+    if (value >= 0.38) return BumpEnum.Both;
+}
 
     this.shutdown = function() {
         //Todo: If we need to shutdown any hardware handles etc.
--- Tests/test5.js
+++ Tests/test5.js
@@ -10,11 +10,12 @@ var eventEmitter = new events.EventEmitter();
 eventEmitter.addListener('bumper', listner1);
 
 var previous = -1;
-var analogPin0 = new mraa.Aio(0); //setup access analog input pin 0
+var analogPin0 = new mraa.Aio(1); //setup access analog input pin 0
 
 //Monitor the bumper analogue pin and raise events if it changes
 setInterval(function () {
     var analogValueFloat = analogPin0.readFloat();
+    //console.log(analogValueFloat );
     var newval = floatToSwitch(analogValueFloat)
     if (newval != previous) {
         eventEmitter.emit('bumper',newval);
@@ -32,11 +33,10 @@ var BumpEnum = {
 //Scale the input to one of our 4 values depending on
 //which of the two switches are pressed
 function floatToSwitch(value) {
-    if (value < 0.45) return BumpEnum.Both;
-    if (value < 0.58) return BumpEnum.Left;
-    if (value < 0.83) return BumpEnum.Right;
-    if (value >= 0.83) return BumpEnum.None;
-    return 4;
+    if (value < 0.1) return BumpEnum.None;
+    if (value < 0.25) return BumpEnum.Left;
+    if (value < 0.38) return BumpEnum.Right;
+    if (value >= 0.38) return BumpEnum.Both;
 }
 
 
